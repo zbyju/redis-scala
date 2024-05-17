868d473c6c670cabb91ce03d5ef8dd53b8f7b840
zbyju <zbynek.jurica@gmail.com>
2024-05-17 14:44:57 +0200

    codecrafters submit [skip ci]

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,11 +1,25 @@
 package codecrafters_redis
 
 import java.net.{InetSocketAddress, ServerSocket}
+import scala.util.Using
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 object Server {
   def main(args: Array[String]): Unit = {
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
     val clientSocket = serverSocket.accept()
+
+    Using(clientSocket.getInputStream()) { inputStream =>
+      {
+        val reader = new BufferedReader(new InputStreamReader(inputStream))
+
+        Using(clientSocket.getOutputStream()) { outputStream =>
+          outputStream.write("+PONG\r\n".getBytes())
+        }
+      }
+    }
+    clientSocket.close()
   }
 }
